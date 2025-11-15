ed3193fd31b2fd3c50deb86cb72db412f7777d58
Jacob Yang <jacob.yangg9@gmail.com>
2025-11-14 20:13:40 -0800

    Created the dashboard structure

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -3,8 +3,16 @@ import './Dashboard.css'
 function Dashboard() {
     return(
         <>
+            <div className="dashboardMenu">
+                <h1 id="dashboardMenuBigText">Welcome, Jacob</h1>
+                <button className="tabButtons" id="tabButtonOne">My Sets</button>
+                <button className="tabButtons">Create Set</button>
+            </div>
             <div>
-                <h1>LOOK AT ME!!!</h1>
+
+            </div>
+            <div>
+
             </div>
         </>
     )
