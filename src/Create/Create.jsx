653eda252a4f1d6738ae7cf90b7102e7f2950d36
Jacob Yang <jacob.yangg9@gmail.com>
2025-11-22 13:38:28 -0800

    Fixed Create.css styling issue

--- src/Create/Create.jsx
+++ src/Create/Create.jsx
@@ -3,9 +3,18 @@ import styles from '../Create/Create.module.css';
 function Create() {
     return (
         <>
-            <div className={styles.createHeader}>
-                <h1>Create a new set</h1>
-                <button>Create</button>
+            <div className={styles.createContainer}>
+                <div className={styles.header}>
+                    <div className={styles.headerMessageWrapper}>
+                        <h1 className={styles.headerMessage}>Create a new set</h1>
+                    </div>
+                    <div className={styles.headerButtonWrapper}>
+                        <button className={styles.headerButton}>Create</button>
+                    </div>
+                </div>
+                <div className={styles.statsWrapper}>
+                    <input className={styles.titleInputField} placeholder='Title'/>
+                </div>
             </div>
         </>
     )
